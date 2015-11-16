52894374d5f15b689da26e20c1a8ac8e91e5a92b
David Nieto Cobo <david.nietocobo@gmail.com>
2015-11-16 19:48:58 +0100

    Creado Twitter basico sin log

--- src/main/java/fdi/myproyect/twitter/controladores/TwitterController.java
+++ src/main/java/fdi/myproyect/twitter/controladores/TwitterController.java
@@ -1,8 +1,15 @@
 package fdi.myproyect.twitter.controladores;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import fdi.myproyect.twitter.entidades.TweetEntity;
 import fdi.myproyect.twitter.servicioAplicacion.TwitterSA;
 
 @Controller
@@ -14,6 +21,28 @@ public class TwitterController {
 	public TwitterController(TwitterSA servicio) {
 		this.servicio = servicio;
 	}
+	
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	String add(TweetEntity tweet){
+		servicio.addTweet(tweet);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+		
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		model.put("tweets", servicio.getAllTweets());
+		
+		ModelAndView view = new ModelAndView("home", model);
+
+		
+		return view;
+	}
+	
+
 	
 	
 	
