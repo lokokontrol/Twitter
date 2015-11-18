c267cc7db2f91f3cc5aef2e2bbbc16cd3ba11e1b
David Rico Valverde <davidrico77@gmail.com>
2015-11-18 20:56:35 +0100

    Creada clase UsuarioController sin acabar!

---
+++ src/main/java/fdi/myproyect/twitter/controladores/UsuarioController.java
@@ -0,0 +1,19 @@
+package fdi.myproyect.twitter.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import fdi.myproyect.twitter.servicioAplicacion.UsuarioSA;
+
+@Controller
+public class UsuarioController {
+
+	UsuarioSA servicio;
+	
+	@Autowired
+	public UsuarioController(UsuarioSA servicio)
+	{
+		this.servicio = servicio;
+	}
+	
+}
