ebfe7d7473dd12c20294a0c5ae63f9abad8e850a
David Nieto <david.nietocobo@gmail.com>
2016-01-12 18:43:54 +0100

    TwitterController y TwitterSA  mejorado y corregido.

--- src/main/java/fdi/myproyect/twitter/controladores/TwitterController.java
+++ src/main/java/fdi/myproyect/twitter/controladores/TwitterController.java
@@ -1,9 +1,5 @@
 package fdi.myproyect.twitter.controladores;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,52 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import fdi.myproyect.twitter.entidades.TweetEntity;
-import fdi.myproyect.twitter.entidades.UsuarioEntity;
 import fdi.myproyect.twitter.servicioAplicacion.TwitterSA;
 import fdi.myproyect.twitter.servicioAplicacion.UsuarioSA;
 
 @Controller
 public class TwitterController {
 	
-	TwitterSA servicio ;
+	TwitterSA servicioTwitter ;
 	UsuarioSA servicioUsuario;
 	
 	@Autowired
-	public TwitterController(TwitterSA servicio,UsuarioSA servicioUsuario) {
-		this.servicio = servicio;
+	public TwitterController(TwitterSA servicioTwitter,UsuarioSA servicioUsuario) {
+		this.servicioTwitter = servicioTwitter;
 		this.servicioUsuario = servicioUsuario;
 	}
 	
 	@RequestMapping(value = "/paginaTwitter", method = RequestMethod.GET)
 	public ModelAndView paginaTwitter() {
-		
-		Map<String, Object> model = new HashMap<String, Object>();
-		
-		model.put("tweets", servicio.getAllTweets());
-		
-		
-		ModelAndView view = new ModelAndView("paginaTwitter", model);
-		
 	
-		return view;
+		return servicioTwitter.getAllTweets();
 	}
 
-	
 	@RequestMapping(value="/paginaTwitter", method = RequestMethod.POST)
 	String add(TweetEntity tweet){
 		
-		
-		List<UsuarioEntity> arrayUsu = servicioUsuario.getAllUsuario();
-		
-		Iterator<UsuarioEntity> it = arrayUsu.iterator();
-		
-		while(it.hasNext()){
-		//	Object aux = it.next();
-			UsuarioEntity usuarioAux = it.next();
-			if(usuarioAux.getLogin() == true)
-				tweet.setUsuario(usuarioAux);
-		}
-		servicio.addTweet(tweet);
+		servicioTwitter.addTweet(tweet);
 		
 		return "redirect:/paginaTwitter";
 	}
@@ -65,16 +40,7 @@ public class TwitterController {
 	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
 	public ModelAndView user(@PathVariable(value="id") int id) {
 		
-		Map<String, Object> model = new HashMap<String, Object>();
-		
-		model.put("tweets", servicio.getAllTweetsByUser(id));
-		
-		
-		ModelAndView view = new ModelAndView("tweets", model);
-		
-		
-			
-		return view;
+		return servicioTwitter.getAllTweetsByUser(id);
 	}
 
 	
--- src/main/java/fdi/myproyect/twitter/servicioAplicacion/TwitterSA.java
+++ src/main/java/fdi/myproyect/twitter/servicioAplicacion/TwitterSA.java
@@ -1,14 +1,18 @@
 package fdi.myproyect.twitter.servicioAplicacion;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import fdi.myproyect.twitter.entidades.TweetEntity;
-
+import fdi.myproyect.twitter.entidades.UsuarioEntity;
 import fdi.myproyect.twitter.repositorios.TwitterRepositorio;
 
 @Service
@@ -16,25 +20,55 @@ import fdi.myproyect.twitter.repositorios.TwitterRepositorio;
 public class TwitterSA {
 	
 	TwitterRepositorio repositorio;
+	UsuarioSA servicioUsuario;
 	
 	@Autowired
-	public TwitterSA(TwitterRepositorio repositorio)
+	public TwitterSA(TwitterRepositorio repositorio,UsuarioSA servicioUsuario)
 	{
 		this.repositorio = repositorio;
+		this.servicioUsuario = servicioUsuario;
 	}
 	
 	public void addTweet(TweetEntity tweet){
-	
+		
+		
+		List<UsuarioEntity> arrayUsu = servicioUsuario.getAllUsuario();
+		
+		Iterator<UsuarioEntity> it = arrayUsu.iterator();
+		
+		while(it.hasNext()){
+		//	Object aux = it.next();
+			UsuarioEntity usuarioAux = it.next();
+			if(usuarioAux.getLogin() == true)
+				tweet.setUsuario(usuarioAux);
+		}
+		
 		repositorio.addTweet(tweet);
 	}
 	
-	 public  List<TweetEntity> getAllTweets()
+	 public  ModelAndView getAllTweets()
 	 {
-		 return repositorio.getAllTweets();
+		 
+		 Map<String, Object> model = new HashMap<String, Object>();
+			
+			model.put("tweets", this.repositorio.getAllTweets());
+			
+			ModelAndView view = new ModelAndView("paginaTwitter", model);
+			
+			return view;
 	 }
 	
-	 public List<TweetEntity> getAllTweetsByUser(int id){
-		return this.repositorio.getAllTweetsByUser(id);
+	 public ModelAndView getAllTweetsByUser(int id){
+		//return this.repositorio.getAllTweetsByUser(id);
+		
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		model.put("tweets", this.repositorio.getAllTweetsByUser(id));
+		
+		
+		ModelAndView view = new ModelAndView("tweets", model);
+		
+		return view;
 	 }
 
 }
